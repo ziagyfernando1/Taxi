83600a36e5c9080f1c1eb7f41513f4eb07f0f3b7
ziagyfernando1 <ziagyfernando1@students.amikom.ac.id>
2022-03-26 21:53:31 +0700

    Add project files.

---
+++ Taxi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            //membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //pengesatan nilai propertis
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.Numpassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickupPassanger();
+            taxi.DropOffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi/Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int Numpassenger { get; set; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number Of Passanger: {0}\n", Numpassenger);
+        }
+        public void PickupPassanger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOffPassanger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+        
+    }
+}
